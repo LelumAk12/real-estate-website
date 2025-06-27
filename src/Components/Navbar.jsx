af5bc430c63c855ad6234a155f77d7824bf0671a
LelumAk12 <akalanka12.me@gmail.com>
2025-06-28 04:14:17 +0530

    Mobile Screen Responsiveness

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  
+  useEffect(() => {
+    if (showMobileMenu) {
+      document.body.style.overflow = "hidden";
+    }else {
+      document.body.style.overflow = "auto"; 
+    }return () => {
+      document.body.style.overflow = "auto"; 
+    }
+  }, [showMobileMenu]);
+
+  
   return (
     <div className="absolute top-0 left-0 w-full z-10">
       <div className="container mx-auto flex items-center justify-between py-4 px-6 lg:px-32 bg-transparent">
@@ -23,14 +36,24 @@ const Navbar = () => {
             Testimonials
           </a>
         </ul>
-
-        <div className="hidden md:block">
-          <button className="bg-white px-8 py-2 rounded-full text-black hover:bg-gray-100">
+          <button className="hidden md:block bg-white px-8 py-2 rounded-full">
             Sign Up
           </button>
+          <img onClick={()=> setShowMobileMenu(true)} src={assets.menu_icon} className="md:hidden w-7 cursor-pointer" alt="" />
+        </div>
+        {/*--------mobile-menu---------*/}
+        <div className={`md:hidden ${showMobileMenu ? "fixed w-full" : "h-0 w-0" } right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
+            <div className="flex justify-end p-6 cursor-pointer">
+                <img onClick={()=> setShowMobileMenu(false)} src={assets.cross_icon} className="w-6" alt="" />
+            </div>
+            <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+                <a href="#Header" className="px-4 py-2 rounded-full inline-block">Home</a>
+                <a href="#About" className="px-4 py-2 rounded-full inline-block">About</a>
+                <a href="#Projects" className="px-4 py-2 rounded-full inline-block">Projects</a>
+                <a href="#Testimonials" className="px-4 py-2 rounded-full inline-block">Testimonials</a>
+            </ul>
         </div>
       </div>
-    </div>
   );
 };
 
